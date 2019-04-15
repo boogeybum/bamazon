f9a7676169c5fdb35e35f2517a37b8168fc94932
boogeybum <45719473+boogeybum@users.noreply.github.com>
2019-04-15 16:05:59 -0500

    Added gitignore with node modules. Set up communication to mysql for basic connection

---
+++ bamazonCustomer.js
@@ -0,0 +1,18 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "788@IdmdemMdimdoc",
+    database: "bamazon_db"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    connection.end();
+    // start();
+});
+
